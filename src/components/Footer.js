2dc4afb4eb0e67cc2cb283ae879ca29966321f15
MsCardoso77 <mathsoarescardoso@gmail.com>
2022-06-12 22:42:51 -0300

    feat: add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <p className={styles.footerText}>© 2022 AnaDec, Inc. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
