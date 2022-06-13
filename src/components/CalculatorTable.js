e33913729d5cb48c9ff54049951b78413d04f8df
MsCardoso77 <mathsoarescardoso@gmail.com>
2022-06-12 22:43:05 -0300

    feat: add CalculatorTable component

---
+++ src/components/CalculatorTable.js
@@ -0,0 +1,21 @@
+import { Table } from "react-bootstrap";
+
+const CalculatorTable = ({numberOfScenarios, numberOfInvestments}) => {
+  return (
+    <>
+      <Table striped bordered>
+        <thead>
+          <tr>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  )
+}
+
+export default CalculatorTable
