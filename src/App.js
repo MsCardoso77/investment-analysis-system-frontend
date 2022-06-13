a4cece9b057cc92d52dfa02ee7426c121aa2248c
MsCardoso77 <mathsoarescardoso@gmail.com>
2022-06-12 22:45:45 -0300

    feat: update app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Home from './pages/Home/Home';
+import Calculator from './pages/Calculator/Calculator';
+import Login from './pages/Login/Login';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/calculator" element={<Calculator />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
