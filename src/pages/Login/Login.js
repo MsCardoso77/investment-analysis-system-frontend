e73198c502cc860374a07477df7689c4dd998b75
MsCardoso77 <mathsoarescardoso@gmail.com>
2022-06-14 11:12:35 -0300

    feat: update login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,17 @@
 import styles from './Login.module.css'
+import googleLogin from './googleLogin.svg'
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className={styles.container}>
+      <div className={styles.banner}>
+        <h1 className={styles.bannerTitle}>Ana<span className={styles.bannerTitleSpan}>Dec</span></h1>
+        <p className={styles.bannerParagraph}>Análise de Decisão <br/> aplicada a Investimentos</p>
+      </div>
+      <div className={styles.login}>
+        <h1 className={styles.loginTitle}>Faça a sua análise</h1>
+        <button className={styles.loginButton}><img className={styles.loginGoogleImage} src={googleLogin} alt="Sign in with Google" /></button>
+      </div>
     </div>
   )
 }
