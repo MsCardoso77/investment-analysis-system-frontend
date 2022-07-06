1627741f832112836b9a773774c8d1e90374f803
MsCardoso77 <mathsoarescardoso@gmail.com>
2022-07-06 00:09:49 -0300

    feat: add ListarRelatórios component on home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,11 @@
+import ListarRelatórios from '../../components/ListarRelatórios'
 import styles from './Home.module.css'
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Relatórios</h1>
+      <ListarRelatórios />
     </div>
   )
 }
