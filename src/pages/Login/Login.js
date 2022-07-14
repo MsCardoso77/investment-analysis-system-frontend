e69ac7d76db5aca53eb87de0d6310e5c36955976
Erickinhou <willianerick17@gmail.com>
2022-07-13 22:52:27 -0300

    remove react-login component

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,14 +1,44 @@
+/* eslint-disable no-undef */
 import styles from "./Login.module.css";
-import googleLogin from "./googleLogin.svg";
-import GoogleLogin from "react-google-login";
+import { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
+import axios from "axios";
 
 const Login = () => {
-  const responseSuccessGoogle = (data) => {
-    console.log("success", data);
+  const handleCallbackResponse = async (response) => {
+    const userObject = jwt_decode(response.credential);
+    try {
+      console.log("register");
+      const result = await axios.post("http://localhost:3333/user/register", {
+        email: userObject.email,
+        password: userObject.sub,
+        name: userObject.name,
+      });
+      console.log("data login result", result.data);
+      sessionStorage.setItem("id_user", result.data?.id);
+    } catch (err) {
+      console.log("login");
+      const result = await axios.post("http://localhost:3333/user/login", {
+        email: userObject.email,
+        password: userObject.sub,
+        name: userObject.name,
+      });
+      console.log("data register result", result.data);
+      sessionStorage.setItem("id_user", result.data?.id);
+    }
   };
-  const responseErrorGoogle = (data) => {
-    console.log("error", data);
-  };
+
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id:
+        "64145929529-au1ruabftskgmtdjeak5pn1q8abc5fej.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
   return (
     <div className={styles.container}>
       <div className={styles.banner}>
@@ -21,12 +51,7 @@ const Login = () => {
       </div>
       <div className={styles.login}>
         <h1 className={styles.loginTitle}>Faça a sua análise</h1>
-        <GoogleLogin
-          clientId="239413194410-k7b6lucsr99ug3vpf12f5sepr34dfi20.apps.googleusercontent.com"
-          buttonText="Login com google"
-          onSuccess={responseSuccessGoogle}
-          onFailure={responseErrorGoogle}
-        />
+        <div id="signInDiv"></div>
       </div>
     </div>
   );
